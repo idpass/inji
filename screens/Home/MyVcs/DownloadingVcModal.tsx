a3d6f13c71d9d56948b51d5fd449397bdab11039
Nichole Martinez <101313323+nicholemnl@users.noreply.github.com>
2022-04-19 21:10:26 +0800

    Fix screens that got ui issues in IOS

--- screens/Home/MyVcs/DownloadingVcModal.tsx
+++ screens/Home/MyVcs/DownloadingVcModal.tsx
@@ -8,7 +8,7 @@ export const DownloadingVcModal: React.FC<ModalProps> = (props) => {
 
   return (
     <Modal isVisible={props.isVisible} onDismiss={props.onDismiss}>
-      <Column fill padding="32 24" align="space-between">
+      <Column fill pY={32} pX={24} align="space-between">
         <Column fill>
           <Text weight="semibold" align="center">
             Downloading your {controller.vcLabel.singular}
--- screens/Home/MyVcs/IdInputModal.tsx
+++ screens/Home/MyVcs/IdInputModal.tsx
@@ -5,7 +5,7 @@ import { Button, Centered, Column, Row, Text } from '../../../components/ui';
 import { Modal } from '../../../components/ui/Modal';
 import { Colors } from '../../../components/ui/styleUtils';
 import { IdInputModalProps, useIdInputModal } from './IdInputModalController';
-import { SafeAreaView, View } from 'react-native';
+import { Platform } from 'react-native';
 
 export const IdInputModal: React.FC<IdInputModalProps> = (props) => {
   const controller = useIdInputModal(props);
@@ -13,58 +13,56 @@ export const IdInputModal: React.FC<IdInputModalProps> = (props) => {
 
   return (
     <Modal onDismiss={props.onDismiss} isVisible={props.isVisible}>
-      <Column padding="32 24" style={{ height: '100%', overflow: 'scroll' }}>
-        <View style={{ height: controller.idInputRef ? '25%': '80%' }}>
-          <Text align="center">
-            Enter the MOSIP-provided UIN or VID{'\n'}of the{' '}
-            {controller.vcLabel.singular} you wish to retrieve
-          </Text>
-        </View>
-      </Column>
-
-      <Column>
-        <Row crossAlign="flex-end">
-          <Column
-            width="33%"
-            style={{
-              borderBottomWidth: 1,
-              borderColor: Colors.Grey,
-              bottom: 24,
-            }}>
-            <Picker
-              selectedValue={controller.idType}
-              onValueChange={controller.SELECT_ID_TYPE}>
-              <Picker.Item label="UIN" value="UIN" />
-              <Picker.Item label="VID" value="VID" />
-            </Picker>
-          </Column>
-          <Column fill>
-            <Input
-              placeholder={!controller.id ? inputLabel : ''}
-              label={controller.id ? inputLabel : ''}
-              labelStyle={{
-                color: controller.isInvalid ? Colors.Red : Colors.Black,
-              }}
-              value={controller.id}
-              keyboardType="number-pad"
-              rightIcon={
-                controller.isInvalid ? (
-                  <Icon name="error" size={18} color="red" />
-                ) : null
-              }
-              errorStyle={{ color: Colors.Red }}
-              errorMessage={controller.idError}
-              onChangeText={controller.INPUT_ID}
-              ref={(node) => !controller.idInputRef && controller.READY(node)}
-            />
-          </Column>
-        </Row>
-        <Button
-          title={`Generate ${controller.vcLabel.singular}`}
-          margin="24 0 0 0"
-          onPress={controller.VALIDATE_INPUT}
-          loading={controller.isRequestingOtp}
-        />
+      <Column fill padding="32 24">
+        <Text align="center" style={{ flex: Platform.OS === 'ios' ? 0.4 : 0.9 }}>
+          Enter the MOSIP-provided UIN or VID{'\n'}of the{' '}
+          {controller.vcLabel.singular} you wish to retrieve
+        </Text>
+        <Column>
+          <Row crossAlign="flex-end">
+            <Column
+              width="33%"
+              style={{
+                borderBottomWidth: 1,
+                borderColor: Platform.OS === 'ios' ? 'transparent' : Colors.Grey,
+                bottom: Platform.OS === 'ios' ? 50 : 24,
+                height: Platform.OS === 'ios' ? 100 : 'auto'
+              }}>
+              <Picker
+                selectedValue={controller.idType}
+                onValueChange={controller.SELECT_ID_TYPE}>
+                <Picker.Item label="UIN" value="UIN" />
+                <Picker.Item label="VID" value="VID" />
+              </Picker>
+            </Column>
+            <Column fill>
+              <Input
+                placeholder={!controller.id ? inputLabel : ''}
+                label={controller.id ? inputLabel : ''}
+                labelStyle={{
+                  color: controller.isInvalid ? Colors.Red : Colors.Black,
+                }}
+                value={controller.id}
+                keyboardType="number-pad"
+                rightIcon={
+                  controller.isInvalid ? (
+                    <Icon name="error" size={18} color="red" />
+                  ) : null
+                }
+                errorStyle={{ color: Colors.Red }}
+                errorMessage={controller.idError}
+                onChangeText={controller.INPUT_ID}
+                ref={(node) => !controller.idInputRef && controller.READY(node)}
+              />
+            </Column>
+          </Row>
+          <Button
+            title={`Generate ${controller.vcLabel.singular}`}
+            margin="24 0 0 0"
+            onPress={controller.VALIDATE_INPUT}
+            loading={controller.isRequestingOtp}
+          />
+        </Column>
       </Column>
     </Modal>
   );
