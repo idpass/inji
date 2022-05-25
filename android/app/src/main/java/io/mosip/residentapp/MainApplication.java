977842ee8f088a8527cf4ccddc9bc14d15a45b68
Paolo Miguel de Leon <paolo@newlogic.com>
2022-05-25 18:25:06 +0800

    feat: add intent for sharing VC data with ODK

--- android/app/src/main/java/io/mosip/residentapp/MainApplication.java
+++ android/app/src/main/java/io/mosip/residentapp/MainApplication.java
@@ -18,7 +18,6 @@ import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
 import timber.log.Timber;
 
-import com.facebook.react.bridge.JSIModulePackage;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
@@ -39,7 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
+      packages.add(new ODKIntentPackage());
       return packages;
     }
 
