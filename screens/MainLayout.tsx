56a414e7bf96b1cf17b88feb5c09df40b0c21cc6
Paolo Miguel de Leon <paolo@newlogic.com>
2024-01-30 15:49:11 +0800

    fix: typos

--- screens/MainLayout.tsx
+++ screens/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect }, {useContext} from 'react';
+import React, { useContext, useEffect } from 'react';
 import {
   BottomTabNavigationOptions,
   createBottomTabNavigator,
